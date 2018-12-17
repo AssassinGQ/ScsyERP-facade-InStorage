c1f6a7aa60b0da4d413a26d230d382c9e58661be
AssassinG <hangq_zju@hotmail.com>
2018-12-17 15:34:49 +0800

    push

--- src/main/java/cn/AssassinG/ScsyERP/InStorage/facade/service/InStorageFormServiceFacade.java
+++ src/main/java/cn/AssassinG/ScsyERP/InStorage/facade/service/InStorageFormServiceFacade.java
@@ -4,10 +4,10 @@ import cn.AssassinG.ScsyERP.InStorage.facade.entity.InStorageForm;
 import cn.AssassinG.ScsyERP.common.core.service.FormService;
 
 public interface InStorageFormServiceFacade extends FormService<InStorageForm> {
-    void addDriveWorkers(Long entityId, String jsonArrayStr);
-    void removeDriveWorker(Long entityId, Long driveWorkerId);
-    void addLiftWorkers(Long entityId, String jsonArrayStr);
-    void removeLiftWorker(Long entityId, Long liftWorkerId);
+//    void addDriveWorkers(Long entityId, String jsonArrayStr);
+//    void removeDriveWorker(Long entityId, Long driveWorkerId);
+//    void addLiftWorkers(Long entityId, String jsonArrayStr);
+//    void removeLiftWorker(Long entityId, Long liftWorkerId);
     void addProduct(Long entityId, Long productId);
     void removeProduct(Long entityId, Long productId);
     void updateProductLocation(Long entityId, String Location);
